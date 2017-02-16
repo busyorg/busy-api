ac4694a96669f4e19221b0117b3cae375e46a7a1
adcpm <fabien@bonustrack.co>
2017-02-16 23:51:53 +0700

    Get logged user details

--- helpers/strategy.js
+++ helpers/strategy.js
@@ -1,13 +1,25 @@
 const passport = require('passport');
 const Strategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const db = require('./db');
 const params = {
   secretOrKey: process.env.JWT_SECRET,
   jwtFromRequest: ExtractJwt.fromUrlQueryParameter('token'),
 };
 
 const strategy = new Strategy(params, (payload, done) => {
-  return done(null, payload);
+  const query = 'SELECT * FROM users WHERE "email" = ${email} LIMIT 1';
+  db.query(query, { email: payload.email })
+    .then(users => {
+      if (users[0]) {
+        const user = {
+          id: users[0].id,
+          email: users[0].email,
+        };
+        return done(null, user);
+      }
+    })
+    .catch(err => done(err, null));
 });
 
 passport.use(strategy);
