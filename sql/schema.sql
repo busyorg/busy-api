3af88b5293ef735413a02d489aa0c1770e5be7fc
adcpm <fabien@bonustrack.co>
2017-02-16 23:52:52 +0700

    Update schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,7 +3,11 @@ CREATE TABLE IF NOT EXISTS "users"(
   "id" SERIAL PRIMARY KEY,
   "email" TEXT NOT NULL UNIQUE,
   "password" TEXT NOT NULL,
-  "phone" SERIAL NOT NULL UNIQUE,
-  "email_is_verified" INT DEFAULT 0,
-  "phone_is_verified" INT DEFAULT 0,
+  "phone" SERIAL NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS "accounts"(
+  "id" SERIAL PRIMARY KEY,
+  "user" INT REFERENCES users(id),
+  "username" TEXT NOT NULL,
 );
