725f9af6e3b16e41cb9ec84fde435c173b3e95d5
adcpm <fabien@bonustrack.co>
2017-04-05 01:35:31 +0700

    Save random secret for user, send email with secret and add route for confirm email.

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 const bcrypt = require('bcrypt');
 const validator = require('validator');
 const router = express.Router();
@@ -16,8 +17,10 @@ router.get('/me', passport.authenticate('jwt', { session: false }), function(req
 router.get('/signup', async function(req, res, next) {
   const email = req.query.email;
   const password = req.query.password;
+
   const query = 'SELECT * FROM users WHERE "email" = ${email} LIMIT 1';
   const users = await req.db.query(query, { email });
+
   if (users[0]) {
     res.status(400).json({ email: ['Email already exists.'] });
   } else if (!validator.isEmail(email)) {
@@ -26,19 +29,30 @@ router.get('/signup', async function(req, res, next) {
     res.status(400).json({ password: ['Passwords must be between 6 and 128 characters.'] });
   } else {
     const hash = bcrypt.hashSync(password, 10);
-    const query = 'INSERT INTO users(email, password) values(${email}, ${hash})';
-    req.db.none(query, { email, hash });
+    const secret = crypto.randomBytes(32).toString('hex');
+
+    const query = 'INSERT INTO users(email, password, secret) values(${email}, ${hash}, ${secret})';
+    req.db.none(query, { email, hash, secret });
     const token = jwt.sign({ email }, process.env.JWT_SECRET);
     res.json({ token });
+
+    req.mail.send('confirm_email', {
+      url: `https://busy.org/confirm?email=${email}&code=${secret}`
+    }, (err, result) => {
+      console.log(err, result);
+    });
   }
 });
 
 router.get('/login', async function(req, res, next) {
   const email = req.query.email;
   const password = req.query.password;
+
   // TODO check if email and password are not empty
+
   const query = 'SELECT * FROM users WHERE "email" = ${email} LIMIT 1';
   const users = await req.db.query(query, { email });
+
   if (!users[0]) {
     res.status(400).json({ email: ['Email does not exist.'] });
   } else if (!bcrypt.compareSync(password, users[0].password)) {
@@ -46,6 +60,22 @@ router.get('/login', async function(req, res, next) {
   } else {
     const token = jwt.sign({ email }, process.env.JWT_SECRET);
     res.json({ token });
+  }
+});
+
+router.get('/confirm', async function(req, res, next) {
+  const email = req.query.email;
+  const code = req.query.code;
+
+  const query = 'SELECT * FROM users WHERE "email" = ${email} AND "secret" = ${code} LIMIT 1';
+  const users = await req.db.query(query, { email, code });
+
+  if (!users[0]) {
+    res.status(400).json({ code: ['Code is not valid.'] });
+  } else {
+    const update = 'UPDATE users SET "email_verified" = 1 WHERE "email" = ${email} AND "secret" = ${code}';
+    req.db.none(update, { email, code });
+    res.json({ success: true });
   }
 });
 
