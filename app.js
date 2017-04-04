f80f30fdc7f2c1a768699010a7a327a5e636a2e7
adcpm <fabien@bonustrack.co>
2017-04-04 19:23:48 +0700

    Add CORS

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(cors());
+
 app.use((req,res,next) => {
   req.db = db;
   next();
