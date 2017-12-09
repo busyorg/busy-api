a5574bb2fdf06aefbc78bb8fa68b34682eb17a9a
adcpm <fabien@bonustrack.co>
2017-12-10 03:51:04 +0700

    Add lightrpc

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 const express = require('express');
-const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const http = require('http');
 const https = require('https');
+const cors = require('cors');
+const { createClient } = require('lightrpc');
+const rpc = require('./routes/rpc');
+
+const client = createClient('https://api.steemit.com');
 http.globalAgent.maxSockets = Infinity;
 https.globalAgent.maxSockets = Infinity;
-const cors = require('cors');
-const passport = require('passport');
-const strategy = require('./helpers/strategy');
-const db = require('./helpers/db');
-const mail = require('./helpers/mail');
-
-const auth = require('./routes/auth');
-const accounts = require('./routes/accounts');
-const translate = require('./routes/translate');
 
 const app = express();
 
@@ -23,16 +18,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cors());
-
 app.use((req,res,next) => {
-  req.db = db;
-  req.mail = mail;
+  req.client = client;
   next();
 });
 
-app.use('/v1/auth', auth);
-app.use('/v1/accounts', accounts);
-app.use('/v1/translate', translate);
+app.use('/', rpc);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -46,6 +37,8 @@ app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  console.error(err);
 
   // render the error page
   res.status(err.status || 500);
