5eee9f3faa40d37466d7f73e0d51560635dc36d6
adcpm <fabien@bonustrack.co>
2017-12-10 03:50:14 +0700

    Remove unused files

--- sql/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users"(
-  "id" SERIAL PRIMARY KEY,
-  "email" TEXT NOT NULL UNIQUE,
-  "password" TEXT NOT NULL,
-  "secret" TEXT NOT NULL,
-  "email_verified" INT DEFAULT 0
-);
