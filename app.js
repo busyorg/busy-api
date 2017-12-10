3428fb966f3d2a7d29cfb88c079549217d93f583
adcpm <fabien@bonustrack.co>
2017-12-10 20:36:10 +0700

    Add redis

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const http = require('http');
 const https = require('https');
 const cors = require('cors');
 const { createClient } = require('lightrpc');
+const redis = require('./helpers/redis');
 const rpc = require('./routes/rpc');
 
 const client = createClient('https://api.steemit.com');
@@ -19,6 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cors());
 app.use((req,res,next) => {
+  req.redis = redis;
   req.client = client;
   next();
 });
