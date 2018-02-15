f41dd5c5475461eb0927652f0a473a323f45a678
adcpm <fabien@bonustrack.co>
2018-02-15 18:15:49 +0700

    Simple cache

--- server.js
+++ server.js
@@ -7,14 +7,23 @@ const steemdWsUrl = process.env.STEEMD_WS_URL || 'wss://rpc.buildteam.io';
 const server = express().listen(port, () => console.log(`Listening on ${port}`));
 const wss = new SocketServer({ server });
 const client = new Client(steemdWsUrl);
+const cache = {};
 
 wss.on('connection', (ws) => {
   console.log('Got connection from new peer');
   ws.on('message', (message) => {
     console.log('Message', message);
     const call = JSON.parse(message);
-    client.call(call.method, call.params, (err, result) => {
-      ws.send(JSON.stringify({ id: call.id, result }));
-    });
+    const key = new Buffer(JSON.stringify([call.method, call.params])).toString('base64');
+    if (!cache[key]) {
+      client.call(call.method, call.params, (err, result) => {
+        ws.send(JSON.stringify({ id: call.id, result }));
+        cache[key] = result;
+      });
+    } else {
+      ws.send(JSON.stringify({ id: call.id, result: cache[key] }));
+    }
   });
+  ws.on('error', () => console.log('Error on connection with peer'));
+  ws.on('close', () => console.log('Connection with peer closed'));
 });
